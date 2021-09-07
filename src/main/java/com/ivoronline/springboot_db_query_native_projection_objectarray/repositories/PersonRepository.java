28291014bc87e3eee0871d898004c4c6ca145c50
igoreta <ivoronline@gmail.com>
2021-09-07 15:03:21 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_db_query_native_projection_objectarray/repositories/PersonRepository.java
+++ src/main/java/com/ivoronline/springboot_db_query_native_projection_objectarray/repositories/PersonRepository.java
@@ -10,10 +10,7 @@ public interface PersonRepository extends CrudRepository<Person, Integer> {
   // RETURN OBJECT ARRAY
   //=======================================================================================
   // ["John",20]
-  @Query(
-    value = "SELECT name, age FROM PERSON WHERE NAME = 'John' AND AGE = 20",
-    nativeQuery = true
-  )
+  @Query(nativeQuery = true, value = "SELECT name, age FROM PERSON WHERE NAME = 'John'")
   Object returnObjectArray();
 
 }
--- src/main/java/com/ivoronline/springboot_db_query_native_projection_objectarray/runners/LoadPersons.java
+++ src/main/java/com/ivoronline/springboot_db_query_native_projection_objectarray/runners/LoadPersons.java
@@ -4,12 +4,10 @@ import com.ivoronline.springboot_db_query_native_projection_objectarray.entities
 import com.ivoronline.springboot_db_query_native_projection_objectarray.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
-@Order(1)
 public class LoadPersons implements CommandLineRunner {
 
   @Autowired
@@ -19,10 +17,6 @@ public class LoadPersons implements CommandLineRunner {
   @Transactional
   public void run(String... args) throws Exception {
     personRepository.save(new Person("John" , 20));
-    personRepository.save(new Person("John" , 21));
-    personRepository.save(new Person("Bill" , 30));
-    personRepository.save(new Person("Nancy", 40));
-    personRepository.save(new Person("Susan", 50));
   }
 
 }
