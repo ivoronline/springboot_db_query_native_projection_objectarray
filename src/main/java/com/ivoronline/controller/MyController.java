9dd059540971717ba564f732d918d623a8e0f87a
ivoronline <ivoronline@gmail.com>
2024-08-19 20:22:54 +0200

    Commit

--- src/main/java/com/ivoronline/controller/MyController.java
+++ src/main/java/com/ivoronline/controller/MyController.java
@@ -4,6 +4,7 @@ import com.ivoronline.repository.UtilityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class MyController {
@@ -19,12 +20,33 @@ public class MyController {
   Object getPerson() {
   
     //GET COLUMNS
-    Object[] properties = (Object[]) utilityRepository.getColumns();
+    Object[] properties = (Object[]) utilityRepository.getPerson();
     
     //DISPLAY COLUMNS
-    String  name = (String)  properties[0];
+    String  name = (String ) properties[0];
     Integer age  = (Integer) properties[1];
     System.out.println(name + ", " + age);
+    
+    //RETURN OBJECT ARRAY
+    return properties;
+    
+  }
+
+  //================================================================
+  // GET PERSON LIST
+  //================================================================
+  @RequestMapping("GetPersonList")
+  Object GetPersonList() {
+  
+    //GET COLUMNS
+    List<Object[]> properties = utilityRepository.getPersonList();
+    
+    //DISPLAY COLUMNS
+    for (Object[] property : properties) {
+      String  name = (String ) property[0];
+      Integer age  = (Integer) property[1];
+      System.out.println(name + ", " + age);
+    }
     
     //RETURN OBJECT ARRAY
     return properties;
--- src/main/java/com/ivoronline/repository/UtilityRepository.java
+++ src/main/java/com/ivoronline/repository/UtilityRepository.java
@@ -3,14 +3,22 @@ package com.ivoronline.repository;
 import com.ivoronline.entity.Utility;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
 public interface UtilityRepository extends CrudRepository<Utility, Integer> {
 
   //========================================================================================
-  // GET COLUMNS
+  // GET PERSON
   //=======================================================================================
   // ["Bill", 30]
   @Query(value = "SELECT NAME, AGE FROM PERSON WHERE NAME = 'Bill'", nativeQuery = true)
-  Object getColumns();
+  Object getPerson();
+
+  //========================================================================================
+  // GET PERSON LIST
+  //=======================================================================================
+  // ["Bill", 30]
+  @Query(value = "SELECT NAME, AGE FROM PERSON WHERE NAME = 'John'", nativeQuery = true)
+  List<Object[]> getPersonList();
 
 }
